12f5738cf6e909cf386e0588479b5120eb47f36f
Alok Sardesai <84138698+alokswx@users.noreply.github.com>
2025-03-24 23:12:42 +0530

    Update pointers_1.c

--- conceptual/pointers_1.c
+++ conceptual/pointers_1.c
@@ -8,35 +8,20 @@ int main() {
     
     // first, print out the addresses. These really don't mean nothing,
     // except for the fact that *num2 && **num3 are almost near to each other
-    printf("Value: %d\n", num1);
-    printf("%p\n", (void *)num1);
+    printf("Value: %d and Address: %p\n", num1, &num1);
 
-    printf("Value: %d\n", *num2);
-    printf("%p\n", (void *)num2);
+    printf("Value: %d and Address: %p\n", *num2, &num2);
     
-    printf("Value: %d\n", **num3);
-    printf("%p\n", (void *)num3);
-
-    printf("Now, the interchange happens!!\n");
+    printf("Value: %d and Address: %p\n\n", **num3, &num3);
 
     // Now try changing the original thing by dereferencing once (*num2) and dereferencing twice (*num3) & so on..
     *num2 = 20;
-    printf("%d\n", num1);
-    printf("%d\n", *num2);
-    if (num1 == (*num2)){
-        printf("YES\n");
-    } else {
-        printf("NO\n");
-    }
+    printf("Now, num1 is %d and Address: %p\n", num1, &num1);
+    printf("Also, num2 is %d and Address: %p\n", *num2, &num2);
 
     **num3 = 30;
-    printf("%d\n", num1);
-    printf("%d\n", **num3);
-    if (num1 == (**num3)){
-        printf("YES\n");
-    } else {
-        printf("NO\n");
-    }
+    printf("Now, num1 is %d and Address: %p\n", num1, &num1);
+    printf("Also, num3 is %d and Address: %p\n", **num3, &num3);
 
     return 0;
 }
